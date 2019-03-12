742450cce20e79d8a8d75de13e13145f50fd936a
ANTON LYCH <Luckytoha333@gmail.com>
2019-03-13 00:28:49 +0300

    All tests are done

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  let lengthArray = preferences.length; 
+  let count = 0;  
+  for (let i = 0; i<lengthArray; i++) { 
+    let next1 = preferences[i] - 1;
+    if (next1 == i) continue;
+    let next2 = preferences[next1] - 1;
+    if (next1 == next2) continue;
+    let next3 = preferences[next2] - 1;
+    if (next3 == next2) continue;
+    if (next3 == i) {
+          count++;
+      }
+   }
+   return count / 3;
+}
